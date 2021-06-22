c77fb1c33f372dc84ba1b53c4cc27b5cddb5be98
shwetachauhan-simform <shweta.c@simformsolutions.com>
2021-06-22 17:34:03 +0530

    update : - update theme - update theme

---
+++ app/build/generated/source/buildConfig/debug/com/simform/ssloadingbuttonandroid/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.simform.ssloadingbuttonandroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.simform.ssloadingbuttonandroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
